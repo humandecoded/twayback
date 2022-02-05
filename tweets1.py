39bb0c52dcb1f29f1b04e0daef313378f783f11f
Mennaruuk <52135169+Mennaruuk@users.noreply.github.com>
2022-02-05 00:18:27 -0500

    Delete tweets1.py

--- tweets1.py
+++
@@ -1,7 +0,0 @@
-import os
-import re
-
-with open("sample.txt", "r") as file:
-	for line in file:
-		urls = re.findall('https?://[^\s<>"]+[|www\.^\s<>"]+', line)
-		print(*urls, file=open("temp1.txt", "a"))
