cedd9386ff54779775b4ca9027f60e16fdf466ad
Mennaruuk <52135169+Mennaruuk@users.noreply.github.com>
2022-02-06 14:04:28 -0500

    Update twayback.py

--- twayback.py
+++ twayback.py
@@ -20,8 +20,7 @@ todate = args['todate']
 # Active, suspended, or doesn't exist?
 data1 =f"https://twitter.com/{username}"
 results = []
-headers = {'user-agent':'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)'}
-
+headers = {'user-agent':'Mozilla/5.0 (compatible; DuckDuckBot-Https/1.1; https://duckduckgo.com/duckduckbot)'}
 response = session.get(data1, headers=headers, allow_redirects=False)
 status_code = response.status_code
 
@@ -31,11 +30,10 @@ if status_code == 200:
 elif status_code == 302:
     print(Back.RED + Fore.WHITE + f"Account is SUSPENDED. This means all of {Back.WHITE + Fore.RED + username + Back.RED + Fore.WHITE}'s Tweets will be downloaded.\n")
     time.sleep(3)
-    start = time.time()
 else:
     print(Back.YELLOW + Fore.WHITE + f"No one currently has this handle. Twayback will search for a history of this handle's Tweets.\n")
     time.sleep(2)
-    start = time.time()
+
 stuck = "(Don't worry, Twayback isn't stuck!"
 print(f"Please wait. Twayback is searching far and wide for deleted tweets from {username}.\nDrink some delicious coffee while this gets done.\n\n{Back.MAGENTA + stuck + Fore.WHITE}\nDepending on the number of Tweets, this step might take several minutes.)\n")
 
@@ -43,7 +41,7 @@ link = f"https://web.archive.org/cdx/search/cdx?url=twitter.com/{username}/statu
 data2 = []
 blocklist = []
 c = session.get(link).text
-urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', c)
+urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', c)
 blocks = re.findall(r'Blocked', c)
 
 for block in blocks:
@@ -57,12 +55,44 @@ for block in blocks:
 for url in urls:
     data2.append(f"{url}")
 
-data3 = [g for g in data2 if len(str(g)) < 62]
-
+username_character_count = len(username)
+if username_character_count == 1:
+    data2 = [g for g in data2 if len(str(g)) <= 48]
+elif username_character_count == 2:
+    data2 = [g for g in data2 if len(str(g)) <= 49]
+elif username_character_count == 3:
+    data2 = [g for g in data2 if len(str(g)) <= 50]
+elif username_character_count == 4:
+    data2 = [g for g in data2 if len(str(g)) <= 51]
+elif username_character_count == 5:
+    data2 = [g for g in data2 if len(str(g)) <= 52]
+elif username_character_count == 6:
+    data2 = [g for g in data2 if len(str(g)) <= 53]
+elif username_character_count == 7:
+    data2 = [g for g in data2 if len(str(g)) <= 54]
+elif username_character_count == 8:
+    data2 = [g for g in data2 if len(str(g)) <= 55]
+elif username_character_count == 9:
+    data2 = [g for g in data2 if len(str(g)) <= 56]
+elif username_character_count == 10:
+    data2 = [g for g in data2 if len(str(g)) <= 57]
+elif username_character_count == 11:
+    data2 = [g for g in data2 if len(str(g)) <= 58]
+elif username_character_count == 12:
+    data2 = [g for g in data2 if len(str(g)) <= 59]
+elif username_character_count == 13:
+    data2 = [g for g in data2 if len(str(g)) <= 60]
+elif username_character_count == 14:
+    data2 = [g for g in data2 if len(str(g)) <= 61]
+elif username_character_count == 15:
+    data2 = [g for g in data2 if len(str(g)) <= 62]
+else:
+    pass
+    
 # Remove duplicate URLs
-data4 = list(dict.fromkeys(data3))
+data3 = list(dict.fromkeys(data2))
 
-number_of_elements = len(data4)
+number_of_elements = len(data3)
 if number_of_elements >= 1000:
     print(f"Getting the status codes of {number_of_elements} archived Tweets...\nThat's a lot of Tweets! It's gonna take some time.\nTip: You can use -from and -to to narrow your search between two dates.")
 else:
@@ -72,32 +102,37 @@ else:
 results = []
 headers = {'user-agent':'Mozilla/5.0 (compatible; DuckDuckBot-Https/1.1; https://duckduckgo.com/duckduckbot)'}
 
-for url in track(data4):
+for url in track(data3):
     response = session.get(url, headers=headers)
     status_code = response.status_code
     results.append((url, status_code))
 
 # Append URL and status code
 for url, status_code in results:
-    data4.append(f"{url} {status_code}")
+    data3.append(f"{url} {status_code}")
 
 # Filter for only deleted Tweets
-data5 = [g for g in data4 if " 404" in g]
-data6 = [g.replace(' 404', '') for g in data5]
+data4 = [g for g in data3 if " 404" in g]
+data5 = [g.replace(' 404', '') for g in data4]
+data6 = [g for g in data5 if "," not in g ]
+os.system('cls')
 
 number_of_elements = len(data6)
 
 if number_of_elements == 1:
     answer = input(f"\n{number_of_elements} deleted Tweet has been found.\nWould you like to download it? Type yes or no. Then press Enter. \n")
 elif number_of_elements == 0:
-    answer = input(f"No deleted Tweets have been found.\nTry expanding the date range to check for more Tweets.\n")
+    answer = f"No deleted Tweets have been found.\nTry expanding the date range to check for more Tweets.\n"
+    exit()
 else:
     answer = input(f"\n{number_of_elements} deleted Tweets have been found.\nWould you like to download them all? Type yes or no. Then press Enter. \n")
+
+os.system('cls')
 
 # Use waybackpack to download URLs
 if answer.lower() == 'yes':
     for url in tqdm(data6, position=0, leave=True):
-        subprocess.run(f"waybackpack -d {username} {url}", text=True, capture_output=True)
+        subprocess.run(f"waybackpack -d {username}  --uniques-only --raw {url}", text=True, capture_output=True)
         with open(f'{username}/{username}.txt', 'w') as file:
             for row in data6:
                 s = "".join(map(str, row))
