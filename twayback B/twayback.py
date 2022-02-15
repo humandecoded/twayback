804ce7d90a0e0231c78ffcfba91bffcdad187677
Mennaruuk <52135169+Mennaruuk@users.noreply.github.com>
2022-02-15 12:01:22 -0500

    Add while loop to 'text'

--- twayback B/twayback.py
+++ twayback B/twayback.py
@@ -150,14 +150,24 @@ elif answer.lower() == 'text':
     textlist = []
     textonly = []
     for url in tqdm(wayback, position=0, leave=True):
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding": "*", "Connection": "keep-alive"}
-        response2 = requests.get(url, allow_redirects=False, headers=headers).text
-        regex = re.compile('.*TweetTextSize TweetTextSize--jumbo.*')
-        try:
-            tweet = bs4.BeautifulSoup(response2, "lxml").find("p", {"class": regex}).getText()
-            textonly.append(tweet + "\n\n---")
-        except AttributeError:
-            pass
+        while True:
+            try:
+                headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding": "*", "Connection": "keep-alive"}
+                response2 = requests.get(url, allow_redirects=False, headers=headers).text
+                regex = re.compile('.*TweetTextSize TweetTextSize--jumbo.*')
+                try:
+                    tweet = bs4.BeautifulSoup(response2, "lxml").find("p", {"class": regex}).getText()
+                    textonly.append(tweet + "\n\n---")
+                except AttributeError:
+                    pass
+            except:
+                print("There is a problem with the connection.\n")
+                time.sleep(0.5)
+                print("Either the Wayback Machine is down or it's refusing the requests.\nYour Wi-Fi connection may also be down.")
+                time.sleep(1)
+                print("Retrying...")
+                continue
+            break
     textlist = zip(data5, textonly)
     directory = pathlib.Path(username)
     directory.mkdir(exist_ok=True)
